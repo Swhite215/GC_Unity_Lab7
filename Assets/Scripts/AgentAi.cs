54450daa36607064d7627c5040a704a6bcb9e920
Spencer White <Spencer.White@carat.com>
2018-03-10 14:20:24 -0500

    Mech walk animation, shot, and navmesh added

---
+++ Assets/Scripts/AgentAi.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class AgentAi : MonoBehaviour {
+
+	public GameObject shot;
+	public GameObject shotSpawn;
+	private NavMeshAgent agent;
+	private Transform PlayerTransform;
+
+	public float rechargeTime = 3.0f;
+	private float lastGeneratedTime;
+
+	// Use this for initialization
+	void Start () {
+		GameObject PlayerObject = GameObject.FindGameObjectWithTag ("PlayerTarget");
+		PlayerTransform = PlayerObject.GetComponent<Transform>();
+		agent = GetComponent<NavMeshAgent>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		agent.SetDestination(PlayerTransform.position);
+
+		if (Time.time > lastGeneratedTime + rechargeTime) {
+			fireShot (PlayerTransform.gameObject);
+			lastGeneratedTime = Time.time;
+		}
+	}
+
+	void fireShot(GameObject enemy) {
+		GameObject arrow = (GameObject)Instantiate (shot, shotSpawn.transform.position, Quaternion.identity);
+		arrow.GetComponent<Projectile> ().target = enemy.transform;
+	}
+
+}
---
+++ Assets/Scripts/Projectile.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Projectile : MonoBehaviour {
+
+	private Rigidbody rb;
+	public float attackDamage = 10;
+
+	//Speed
+	public float speed;
+
+	//Target (Set by AI Script)
+	public Transform target;
+
+	// Use this for initialization
+	void Start () {
+		rb = GetComponent<Rigidbody> ();
+		rb.velocity = transform.forward * speed;
+		Destroy(gameObject, 3.0f);
+	}
+
+	void FixedUpdate() 
+	{
+		if (target != null) {
+			Vector3 dir = target.position - transform.position;
+			rb.velocity = dir.normalized * speed;
+		} else {
+			Destroy (gameObject);
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+
+}
